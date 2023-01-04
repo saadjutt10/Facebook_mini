c22bf79cd816fe9e2670acd0032995c96e679d3d
saadjutt10 <saadgondal203@gmail.com>
2023-01-04 10:50:44 +0500

    First commit

---
+++ Address.java
@@ -0,0 +1,49 @@
+public class Address implements Comparable<Address> {
+    private String city;
+    private int streetNo;
+    private int houseNo;
+
+    public Address(String city, int streetNo, int houseNo) {
+        this.city = city;
+        this.streetNo = streetNo;
+        this.houseNo = houseNo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getStreetNo() {
+        return streetNo;
+    }
+
+    public void setStreetNo(int streetNo) {
+        this.streetNo = streetNo;
+    }
+
+    public int getHouseNo() {
+        return houseNo;
+    }
+
+    public void setHouseNo(int houseNo) {
+        this.houseNo = houseNo;
+    }
+
+    @Override
+    public int compareTo(Address o) {
+        if (this.city.equals(o.getCity())) {
+            return 0;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Address [city=" + city + ", streetNo=" + streetNo + ", houseNo=" + houseNo + "]";
+    }
+
+}
---
+++ Profile.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Profile {
+    String name;
+    int age;
+    ArrayList<Profile> friends = new ArrayList<>();
+    ArrayList<String> interest = new ArrayList<>();
+    
+
+}
